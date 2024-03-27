04bbc161625aa4b84973e51b89b1214b6b2fe0a6
rana197 <84954750+rana197@users.noreply.github.com>
2024-03-27 13:26:42 +0530

    Create matlab_java_connect.java

---
+++ matlab_java_connect.java
@@ -0,0 +1,29 @@
+package matlab;
+
+import com.mathworks.engine.*;
+import java.util.concurrent.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class matlab_Topsis_Test {
+	private static MatlabEngine eng;
+	public static void main(String[] args) { 
+		try {
+		eng = MatlabEngine.startMatlab();
+		eng.eval("x= [-20:0.1:20];");
+		eng.eval("y= x.^2;");
+		eng.eval("plot(x,y)");
+		Thread.sleep(10000);
+		} catch (InterruptedException ex) {
+			Logger.getLogger(matlab_Topsis_Test.class.getName()).log(Level.SEVERE,null,ex);
+		} catch (EngineException ex) {
+			Logger.getLogger(matlab_Topsis_Test.class.getName()).log(Level.SEVERE,null,ex);
+		} catch (MatlabExecutionException ex) {
+			Logger.getLogger(matlab_Topsis_Test.class.getName()).log(Level.SEVERE,null,ex);
+		} catch (MatlabSyntaxException ex) {
+			Logger.getLogger(matlab_Topsis_Test.class.getName()).log(Level.SEVERE,null,ex);
+		} catch (ExecutionException ex) {
+			Logger.getLogger(matlab_Topsis_Test.class.getName()).log(Level.SEVERE,null,ex);
+		}	 
+	}
+}
+
