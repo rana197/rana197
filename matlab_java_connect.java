15f983dff2860b266548c28f561a0cfd30a443d9
rana197 <84954750+rana197@users.noreply.github.com>
2024-03-27 13:53:15 +0530

    Update matlab_java_connect.java

--- matlab_java_connect.java
+++ matlab_java_connect.java
@@ -8,10 +8,11 @@ public class matlab_Topsis_Test {
 	private static MatlabEngine eng;
 	public static void main(String[] args) { 
 		try {
-		eng = MatlabEngine.startMatlab();
-		eng.eval("x= [-20:0.2:20];");
-		eng.eval("y= x.^2;");
-		eng.eval("plot(x,y)");
+		//eng = MatlabEngine.startMatlab();
+		//eng.eval("x= [-20:0.2:20];");
+		//eng.eval("y= x.^2;");
+		//eng.eval("plot(x,y)");
+		eng.eval("run('C:\\Users\\kk\\Documents\\PHD_NRS_material_all\\8th_sem\\matlab_java_connect\\test.m')");
 		Thread.sleep(10000);
 		} catch (InterruptedException ex) {
 			Logger.getLogger(matlab_Topsis_Test.class.getName()).log(Level.SEVERE,null,ex);
